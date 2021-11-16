d647a4eb920143b3354623f677402d85c181bc94
Fahim334712 <f.fahimcox@gmail.com>
2021-11-16 19:10:18 +0600

    AlmostDone

---
+++ style.js
@@ -0,0 +1,54 @@
+document.getElementById('purchase1').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy1');
+    const confirm = 'Purchased ! Level:Nursery,fee:$50';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase2').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy2');
+    const confirm = 'Purchased ! Level:Primary,fee:$80';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase3').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy3');
+    const confirm = 'Purchased ! Level:Junior School,fee:$120';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase4').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy4');
+    const confirm = 'Purchased ! Level: Secondary,fee:$150';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase5').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy5');
+    const confirm = 'Purchased ! Level:Higher Secondary,fee:$200';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase6').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy6');
+    const confirm = 'Purchased ! Level:University,fee:$500';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase7').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy7');
+    const confirm = 'Purchased ! Level:Microsoft-Word,fee:$100';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase8').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy8');
+    const confirm = 'Purchased ! Level:Microsoft-Excell,fee:$200';
+    Purchase.innerText = confirm;
+})
+document.getElementById('purchase9').addEventListener('click', function () {
+    alert('Are You Sure!You Buy this Course?');
+    const Purchase = document.getElementById('buy9');
+    const confirm = 'Purchased ! Level:Web Development,fee:$500';
+    Purchase.innerText = confirm;
+})
